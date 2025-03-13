db426cf2974c677c6926a83d6827a1665866c1d0
Eduarte <74308809+EduartePaiva@users.noreply.github.com>
2025-03-13 18:35:02 -0300

    feat: implemented gRPC handlers for the transport interface

--- common/errors.go
+++ common/errors.go
@@ -3,6 +3,30 @@ package common
 import (
 	"errors"
 	"net/http"
+
+	"google.golang.org/grpc/status"
+)
+
+var (
+	GRPC_STATUS_CODE_TO_HTTP = map[int]int{
+		0:  http.StatusOK,                  // OK
+		1:  499,                            // CANCELLED (Client Closed Request)
+		2:  http.StatusInternalServerError, // UNKNOWN
+		3:  http.StatusBadRequest,          // INVALID_ARGUMENT
+		4:  http.StatusGatewayTimeout,      // DEADLINE_EXCEEDED
+		5:  http.StatusNotFound,            // NOT_FOUND
+		6:  http.StatusConflict,            // ALREADY_EXISTS
+		7:  http.StatusForbidden,           // PERMISSION_DENIED
+		8:  http.StatusTooManyRequests,     // RESOURCE_EXHAUSTED
+		9:  http.StatusBadRequest,          // FAILED_PRECONDITION
+		10: http.StatusConflict,            // ABORTED
+		11: http.StatusBadRequest,          // OUT_OF_RANGE
+		12: http.StatusNotImplemented,      // UNIMPLEMENTED
+		13: http.StatusInternalServerError, // INTERNAL
+		14: http.StatusServiceUnavailable,  // UNAVAILABLE
+		15: http.StatusInternalServerError, // DATA_LOSS
+		16: http.StatusUnauthorized,        // UNAUTHENTICATED
+	}
 )
 
 func HandleHttpError(err error, w http.ResponseWriter, fallbackErrorCode int) {
@@ -12,4 +36,25 @@ func HandleHttpError(err error, w http.ResponseWriter, fallbackErrorCode int) {
 		return
 	}
 	WriteError(w, fallbackErrorCode, errors.New("something went wrong"))
+}
+
+func ConvertGrpcErrorToHttpError(err error) error {
+	if err == nil {
+		return nil
+	}
+	grpcErr, ok := status.FromError(err)
+	if !ok {
+		return HttpError{
+			Code:    http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+	}
+	httpCode, ok := GRPC_STATUS_CODE_TO_HTTP[int(grpcErr.Code())]
+	if !ok {
+		httpCode = http.StatusInternalServerError
+	}
+	return HttpError{
+		Message: grpcErr.Message(),
+		Code:    httpCode,
+	}
 }
--- users-api/transports/transports.go
+++ users-api/transports/transports.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 
 	"github.com/EduartePaiva/kubernetes-authentication-microservices/common"
+	pb "github.com/EduartePaiva/kubernetes-authentication-microservices/common/api"
 	"github.com/EduartePaiva/kubernetes-authentication-microservices/users-api/types"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -70,21 +72,36 @@ func (r *restTransportSvc) GetTokenConfirmation(ctx context.Context, token strin
 	panic("unimplemented")
 }
 
-type gRPCTransportSvc struct{}
+type gRPCTransportSvc struct {
+	conn *grpc.ClientConn
+}
 
 // GetHashedPassword implements types.TransportsService.
 func (g *gRPCTransportSvc) GetHashedPassword(ctx context.Context, password string) (string, error) {
-	panic("unimplemented")
+	c := pb.NewAuthServiceClient(g.conn)
+	res, err := c.GetHashedPassword(ctx, &pb.GetHashedPasswordReq{
+		Password: password,
+	})
+	return res.HashedPassword, common.ConvertGrpcErrorToHttpError(err)
 }
 
 // GetToken implements types.TransportsService.
 func (g *gRPCTransportSvc) GetToken(ctx context.Context, password string, hashedPassword string) (string, error) {
-	panic("unimplemented")
+	c := pb.NewAuthServiceClient(g.conn)
+	res, err := c.GetToken(ctx, &pb.GetTokenReq{
+		Password:       password,
+		HashedPassword: hashedPassword,
+	})
+	return res.Token, common.ConvertGrpcErrorToHttpError(err)
 }
 
 // GetTokenConfirmation implements types.TransportsService.
 func (g *gRPCTransportSvc) GetTokenConfirmation(ctx context.Context, token string) (bool, error) {
-	panic("unimplemented")
+	c := pb.NewAuthServiceClient(g.conn)
+	res, err := c.GetTokenConfirmation(ctx, &pb.GetTokenConfirmationReq{
+		Token: token,
+	})
+	return res.IsValid, common.ConvertGrpcErrorToHttpError(err)
 }
 
 func NewTransportService(transportProtocol string) types.TransportsService {
